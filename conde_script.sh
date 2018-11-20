a5d31342d1f9075182aec0db04badc082659c669
michael.babatunde <michael.babatunde@kingfisher.com>
2018-11-20 05:36:36 +0000

    added functions to script

--- conde_script.sh
+++ conde_script.sh
@@ -12,7 +12,7 @@ APP_CLUSTER_STACK_NAME='APP-CLUSTER-STACK'
 APP_DEPLOYMENT_STACK='HELLO-WORLD-APP'
 AWS_REGION='eu-west-1'
 DOCKER_IMAGE_NAME='hello-world-app'
-DOCKERFILE_LOCATION="${PWD}/sample-nodejs-app/."
+DOCKERFILE_LOCATION="${PWD}/hello_world/."
 AWS_ACCOUNT_ID="519912063470"
 DOCKER_IMAGE_TAG="latest"
 #DOCKER_IMAGE_REPO="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
@@ -36,37 +36,61 @@ DOCKER_IMAGE_REPO='519912063470.dkr.ecr.eu-west-1.amazonaws.com/hello-world-app:
 #echo ""
 ######### AWS CONFIGURE END #########
 
-# #AWS CREATE REPOSITIRY
-# echo '#### Creating Docker AWS ECR Image Repository ####'
-# aws ecr create-repository --repository-name ${DOCKER_IMAGE_NAME}
-# #
-# # #TOKEN AUTHENTICATION TO ECR REPOSITORY
-# $(aws ecr get-login --no-include-email --region ${AWS_REGION})
-# #
-# echo '#### Building Docker Image ####'
-# docker build -t  ${DOCKER_IMAGE_NAME} ${DOCKERFILE_LOCATION}
-# #
-# # #TAG IMAGE
-# docker tag ${DOCKER_IMAGE_REPO}
-# #
-# echo '#### Pushing Docker Image To ECR Repository ####'
-# docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
-# #
-# # #need to put a conditional on this message so that if the message is only displayed when the image is successfully built and pushed
-# #
-# echo "Image ${DOCKER_IMAGE_NAME} successfully pushed to ECR repository"
-# #
-# echo '#### Creating VPC STACK ####'
-# aws cloudformation create-stack --template-body file://$PWD/vpc.yaml --stack-name ${VPC_STACK_NAME}
-# aws cloudformation wait stack-create-complete --stack-name  ${VPC_STACK_NAME} $STACK_ID_FROM_CREATE_STACK
-# #
-# echo '#### Creating Stack for IAM roles ####'
-# aws cloudformation create-stack --template-body file://$PWD/iam.yaml --stack-name ${IAM_STACK_NAME} --capabilities CAPABILITY_IAM
-# aws cloudformation wait stack-create-complete --stack-name ${IAM_STACK_NAME} $STACK_ID_FROM_CREATE_STACK
-# #
-# echo '#### Creating App cluster and Resources ####'
-# aws cloudformation create-stack --template-body file://$PWD/app-cluster.yaml --stack-name ${APP_CLUSTER_STACK_NAME}
-# aws cloudformation wait stack-create-complete --stack-name ${APP_CLUSTER_STACK_NAME} $STACK_ID_FROM_CREATE_STACK
+function create_ecr_repo() {
+    echo '#### Creating Docker AWS ECR Image Repository ####'
+    aws ecr create-repository --repository-name ${DOCKER_IMAGE_NAME}
 
-echo '#### Creating App ####'
-aws cloudformation create-stack --template-body file://$PWD/hello_world_app.yaml --stack-name "${APP_DEPLOYMENT_STACK}" #--parameters ParameterKey=ImageRepoUrl,ParameterValue=${DOCKER_IMAGE_REPO} 
+    #TOKEN AUTHENTICATION TO ECR REPOSITORY
+    $(aws ecr get-login --no-include-email --region ${AWS_REGION})
+
+}
+
+function manage_docker_image() {
+    echo '#### Building Docker Image ####'
+    docker build -t  ${DOCKER_IMAGE_NAME} ${DOCKERFILE_LOCATION}
+
+    #TAG IMAGE
+    docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+
+    echo '#### Pushing Docker Image To ECR Repository ####'
+    docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+
+    echo "Image ${DOCKER_IMAGE_NAME} successfully pushed to ECR repository"
+}
+
+function create_vpc_stack() {
+    echo '#### Creating VPC STACK ####'
+    aws cloudformation create-stack --template-body file://$PWD/vpc.yaml --stack-name ${VPC_STACK_NAME}
+    aws cloudformation wait stack-create-complete --stack-name  ${VPC_STACK_NAME} $STACK_ID_FROM_CREATE_STACK
+}
+
+function create_iam_roles() {
+    echo '#### Creating Stack for IAM roles ####'
+    aws cloudformation create-stack --template-body file://$PWD/iam.yaml --stack-name ${IAM_STACK_NAME} --capabilities CAPABILITY_IAM
+    aws cloudformation wait stack-create-complete --stack-name ${IAM_STACK_NAME} $STACK_ID_FROM_CREATE_STACK
+}
+
+function create_app_cluster() {
+    echo '#### Creating App cluster and Resources ####'
+    aws cloudformation create-stack --template-body file://$PWD/app-cluster.yaml --stack-name ${APP_CLUSTER_STACK_NAME}
+    aws cloudformation wait stack-create-complete --stack-name ${APP_CLUSTER_STACK_NAME} $STACK_ID_FROM_CREATE_STACK
+}
+
+
+
+function create_app() {
+    echo '#### Creating App ####'
+    aws cloudformation create-stack --template-body file://$PWD/hello_world_app.yaml --stack-name "${APP_DEPLOYMENT_STACK}" #--parameters ParameterKey=ImageRepoUrl,ParameterValue=${DOCKER_IMAGE_REPO}
+
+}
+
+function main() {
+    create_ecr_repo
+    manage_docker_image
+    create_vpc_stack
+    create_iam_roles
+    create_app_cluster
+    create_app
+}
+
+main
