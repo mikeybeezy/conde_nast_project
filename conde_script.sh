740b05c2d87c587bfc509bbc646fd643c541fbc7
michael.babatunde <michael.babatunde@kingfisher.com>
2018-11-20 09:54:39 +0000

    script modifications

--- conde_script.sh
+++ conde_script.sh
@@ -2,38 +2,34 @@
 set -e
 
 ######### PARAMETERS START  #########
-#Add aws config deatails and balnk it OUT
-#AWS Access keyID
-#AWS secret Aaccess Key
 
 VPC_STACK_NAME='VPC-STACK'
 IAM_STACK_NAME='IAM-STACK'
 APP_CLUSTER_STACK_NAME='APP-CLUSTER-STACK'
 APP_DEPLOYMENT_STACK='HELLO-WORLD-APP'
-AWS_REGION='eu-west-1'
+AWS_REGION=''
 DOCKER_IMAGE_NAME='hello-world-app'
 DOCKERFILE_LOCATION="${PWD}/hello_world/."
-AWS_ACCOUNT_ID="519912063470"
+AWS_ACCOUNT_ID=""
 DOCKER_IMAGE_TAG="latest"
 #DOCKER_IMAGE_REPO="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-DOCKER_IMAGE_REPO='519912063470.dkr.ecr.eu-west-1.amazonaws.com/hello-world-app:latest'
+DOCKER_IMAGE_REPO=''
 
 ######### PARAMETERS END #########
 
 ######### AWS CONFIGURE START #########
-#read -sp 'aws_account_id : ' AWS_ACCOUNT_ID
-#echo "Enter the AWS ACCESS KEY ID followed by [ENTER]:"
-#read -sp 'aws_access_key_id : ' ACCESS_KEY_ID
-#read -sp 'aws_secret_access_key : ' SECRET_ACCESS_KEY
-#read -p 'aws_default_region : ' AWS_REGION
+read -sp 'aws_account_id : ' AWS_ACCOUNT_ID
+echo "Enter the AWS ACCESS KEY ID followed by [ENTER]:"
+read -sp 'aws_access_key_id : ' ACCESS_KEY_ID
+echo "Enter the AWS SECRET ACCESS KEY ACCESS KEY ID followed by [ENTER]:"
+read -sp 'aws_secret_access_key : ' SECRET_ACCESS_KEY
+echo "Enter the AWS REGION followed by [ENTER]:"
+read -p 'aws_default_region : ' AWS_REGION
 
-#aws configure set aws_access_key_id $ACCESS_KEY_ID
-#aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
-#aws configure set default.region $AWS_REGION
+aws configure set aws_access_key_id $ACCESS_KEY_ID
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+aws configure set default.region $AWS_REGION
 
-#echo ""
-#echo "***** CLI configure complete *****"
-#echo ""
 ######### AWS CONFIGURE END #########
 
 function create_ecr_repo() {
